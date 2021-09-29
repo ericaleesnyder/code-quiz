81cada6e7875aa8a4dba019591428d072139c089
Erica Snyder <ericaleesnyder@gmail.com>
2021-09-28 17:50:56 -0700

    added form for submitting scores and initials

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,18 @@
-// WHEN the page loads:
-
-// The following elements are shown:
-// - high scores link
-// - timer at 0
-// - title
-// - welcome message
-// - start button
-
-// WHEN the user clicks start
-
-// - Hide/remove title, welcome message, and start button
-
-// - Display question: What keyword can be used to print the type of a value?
-
-// - display buttons with the following text below the question:
-//   - A true
-//   - B string
-//   - C typeof
-//   - D function
-
-// - Start countdown: display "Time: 120" to start.
-// - Update the time display each second counting down.
-
-
-// WHEN the user clicks a button
-// IF the user clicked "C typeof" button
-
-
-// IF the user clicked "A true", "B string", or "D function"
-
-
-// WHEN the time remaining reaches 0 or less
-
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
-
 var startButton = document.querySelector(".start-button");
 var highScores = document.querySelector(".high-scores");
 var timerEl = document.querySelector(".timer-count");
-
 var answerChoices = document.querySelector(".answer-choices");
 var intro = document.querySelector(".intro");
 var answerChoices = document.querySelector(".answer-choices");
-
+var saveScore = document.querySelector(".save-score")
+var scoreList = document.querySelector(".score-list")
+var mainCard = document.querySelector(".main-card")
 var first = document.querySelector(".first");
 var second = document.querySelector(".second");
 var third = document.querySelector(".third");
 var fourth = document.querySelector(".fourth");
-
-var question = document.querySelector("#question")
-
-var timer;
-var timerCount;
-var timerSubtracted;
+var question = document.querySelector("#question");
+var submit = document.querySelector("#submit");
 
 var questionList = [
     {
@@ -136,12 +87,18 @@ var questionList = [
     correctAnswer: "Array"},
 ];
 
+// timer specific variables 
+var timer;
+var timerCount;
+var timerSubtracted;
+
 function countDown () {
     timerCount = 120;
     timerEl.textContent = "Time: " + timerCount;
     
     timer = setInterval(function() {
         if (timerCount <= 0) {
+            enterScore();
             clearInterval(timer);
         } else {
             timerCount--; 
@@ -150,16 +107,17 @@ function countDown () {
     }, 1000);
 }; 
 
+// displayQuestion specific variables 
 var lastQuestionIndex = questionList.length-1;
 var questionIndex = 0
-var q;
+var q; 
 
 function displayQuestion () {
     q = questionList[questionIndex];
     
     answerChoices.style.display="block";
-    
     question.textContent = q.question;
+    
     first.textContent=q.A;
     second.textContent=q.B;
     third.textContent=q.C;
@@ -168,6 +126,28 @@ function displayQuestion () {
     questionIndex++;
 }
 
+function enterScore() {
+    var score = document.querySelector("#score");
+    score.textContent = "Score: " + timerCount;
+
+    clearInterval(timer); //stops the timer at current score
+    
+    mainCard.style.display="none"; // hides question/answer portion
+    saveScore.style.display="block"; // shows the section in which we are building the form;
+}
+
+function viewHighScore (event) {
+   event.preventDefault();
+
+   mainCard.style.display="none";
+   saveScore.style.display="none";
+   
+   scoreList.style.display="block";
+}
+
+highScores.addEventListener("click", viewHighScore);
+submit.addEventListener('click', viewHighScore);
+
 startButton.addEventListener("click", function () {
     intro.style.display="none";
     displayQuestion();
@@ -175,44 +155,65 @@ startButton.addEventListener("click", function () {
 });
 
 first.addEventListener('click', function () {
+    if (questionIndex >= questionList.length || timerCount<= 0){
+        enterScore();
+        return;
+    }
+
     if (first.textContent !== q.correctAnswer) {
         console.log("wrong");
         timerCount = timerCount-10;
-    }
-    else {
+    
+
+    } else {
         console.log("correct")
         displayQuestion();
         }
 });
 
 second.addEventListener('click', function () {
+    if (questionIndex >= questionList.length || timerCount<= 0){
+        enterScore();
+        return;
+    }
+
     if (second.textContent !== q.correctAnswer) {
         console.log("wrong");
         timerCount = timerCount-10;
-    }
-    else {
+    
+    } else {
         console.log("correct")
         displayQuestion();
         }
 });
 
 third.addEventListener('click', function () {
+    if (questionIndex >= questionList.length || timerCount<= 0){
+        enterScore();
+        return;
+    }
+
     if (third.textContent !== q.correctAnswer) {
         console.log("wrong");
         timerCount = timerCount-10;
-    }
-    else {
+
+    } else {
         console.log("correct")
         displayQuestion();
         }
 });
 
 fourth.addEventListener('click', function () {
+    if (questionIndex >= questionList.length || timerCount<= 0){
+        enterScore();
+        return;
+    }
+    
     if (fourth.textContent !== q.correctAnswer) {
         console.log("wrong");
         timerCount = timerCount-10;
-    }
-    else {
+        
+    } else {
         console.log("correct")
         displayQuestion();
         }
